edcb5bba87c9f0cfee0d95aaed2d78977562f5e2
dottjt <julius.reade@gmail.com>
2019-04-04 08:33:22 +1100

    small fix

--- src/events/onGuildMemberAdd.js
+++ src/events/onGuildMemberAdd.js
@@ -1,11 +1,11 @@
-const { introductionMessage } = require('../const/MESSAGE');
+const { welcomeMessage } = require('../const/MESSAGE');
 
 const onGuildMemberAdd = (/* client */) => {
   return function(member) {
     try {
       const channel = member.guild.channels.find(ch => ch.id === process.env.WELCOME_CHANNEL_ID);
       sendChannelMessageHelper(channel, `Welcome to the server, ${member}!`);
-      sendDirectMessageHelper(member, introductionMessage);
+      sendDirectMessageHelper(member, welcomeMessage);
     } catch(error) {
       throw new Error(`${error} - Could not send onGuildMemberAdd message.`);     
     }
