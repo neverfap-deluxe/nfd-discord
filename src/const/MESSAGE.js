5feb88bae2e0c182d3e94bb91081bc7b62d88175
dottjt <julius.reade@gmail.com>
2019-04-06 12:46:06 +1100

    yus

--- src/const/MESSAGE.js
+++ src/const/MESSAGE.js
@@ -173,7 +173,7 @@ const welcomeMessageTwo = (accountabilityChannel) => `
 Thank you for joining the NeverFap Deluxe Discord Channel!\n
 Chances are that you're here to participate in our ${accountabilityChannel} program. In that case, please read the ${accountabilityChannel} rules by typing in \`!accountability\` into the message bar and pressing enter.\n
 These rules are super important, and I will be working with you to ensure that you understand and follow them! Although we're a fun bunch, this is also a very serious program and our focus is to help you overcome porn addiction.\n
-To get started, please type in \`!accountability\` to learn what this channel is all about ^^.
+To get started, please type in \`!accountability\` and press enter to learn what this channel is all about ^^.
 `;
 
 const cheatsheetMessage = new RichEmbed()
