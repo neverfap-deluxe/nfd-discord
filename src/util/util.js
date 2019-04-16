865b34533ef39adc84eecc93e1ce5ce3f342ce97
dottjt <julius.reade@gmail.com>
2019-04-16 18:11:48 +1000

    yus

--- src/util/util.js
+++ src/util/util.js
@@ -37,8 +37,8 @@ const configureLogger = (Winston) => {
       // - Write to all logs with level `info` and below to `combined.log` 
       // - Write all logs error (and below) to `error.log`.
       //
-      new Winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new Winston.transports.File({ filename: 'combined.log' })
+      new Winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+      new Winston.transports.File({ filename: 'logs/combined.log' })
     ]
   });
   
