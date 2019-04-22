057e3d69e64f71b5b5192c62c47d8a202ae708c9
dottjt <julius.reade@gmail.com>
2019-04-22 15:14:46 +1000

    yus

--- src/events/onMessageReactionAdd/reactTallyUpdate.js
+++ src/events/onMessageReactionAdd/reactTallyUpdate.js
@@ -6,46 +6,46 @@ const knex = require('../../db/knex');
 // const message1 = (discordUser, accountabilityChannel) => new RichEmbed().setTitle("1st #accountability react today").setDescription(
 // `${discordUser} just posted our first ${accountabilityChannel} react for today! Hooray! :grin:`
 // );
-const message50 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`50th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message50 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 50 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `We just reached 50 emoji reacts in ${accountabilityChannel}! Thank you ${discordUser} for the 50th one! :smile:`
 );
-const message100 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`100th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message100 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 100 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `OMG 100 emoji reacts in ${accountabilityChannel}! What a time to be alive! :white_sun_rain_cloud:`
 );
-const message150 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`150th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message150 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 150 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `I can't believe it but we reached 150 emoji reacts today in ${accountabilityChannel}! :yum:`
 );
-const message200 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`200th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message200 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 200 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `Did someone just order 200 emoji reacts in ${accountabilityChannel}!?!??!! :pizza:`
 );
-const message250 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`250th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message250 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 250 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `We're up to 250 emoji reacts in ${accountabilityChannel}! Wowweee!!!! :stuck_out_tongue_winking_eye:`
 );
-const message300 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`300th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message300 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 300 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `It looks like we have 300 emoji reacts in ${accountabilityChannel}! How on earth did we do it! :innocent:`
 );
-const message350 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`350th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message350 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 350 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `OMFG 350 emoji reacts in ${accountabilityChannel}! Surely I'm halucinating right now! :rainbow:`
 );
-const message400 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`400th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message400 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 400 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `The database is telling me that we're currently at 400 emoji reacts in ${accountabilityChannel} for today! WTF! :neutral_face:`
 );
-const message450 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`450th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message450 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 450 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `Could it be!?!?! Obviously, because this is an automated message and I'm the all-knowing all-seeing bot, but we just hit 450 emoji reacts in ${accountabilityChannel}! :game_die:`
 );
-const message500 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`500th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message500 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 500 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `Thank you ${discordUser} for reacting with our 500th emoji today in ${accountabilityChannel}! :bow:`
 );
-const message550 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`550th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message550 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 550 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `Wow! 550 emojis reacted with today in ${accountabilityChannel}! You amazing people really have to stop hehe :doughnut:`
 );
-const message600 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`600th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message600 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 600 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `I actually can't believe it! 600 emoji reacts in ${accountabilityChannel}, did I just die and go to heaven!?!?! :star2:`
 );
-const message650 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`650th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message650 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 650 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `Surely this is just an illusion... 650 emoji reacts in ${accountabilityChannel} and counting! :stuck_out_tongue_closed_eyes:`
 );
-const message700 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`700th emoji react! Only ${hoursTillCountdown} to go!`).setDescription(
+const message700 = (discordUser, accountabilityChannel, hoursTillCountdown) => new RichEmbed().setTitle(`We just hit 700 emoji reacts for today! Only ${hoursTillCountdown} to go!`).setDescription(
 `We did it folks! We hit 700 emoji reacts in ${accountabilityChannel} and I haven't written anymore of these messages! Well done! :trophy:`
 );
 
